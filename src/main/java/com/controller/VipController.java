35fd1189a2e9f96635c2cff216a89aa4abd370a3
zl <zl@LAPTOP-JTUNEV7G>
2018-12-28 11:36:40 +0800

    曾磊

--- src/main/java/com/controller/VipController.java
+++ src/main/java/com/controller/VipController.java
@@ -6,13 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VipController {
 
-    @RequestMapping("/index.htm")
-    public String index(){
-         return "right";
-     }
-
-    @RequestMapping("/w.htm")
-    public String w(){
-        return "women";
-    }
+    
+    
 }
