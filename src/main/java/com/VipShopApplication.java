bacd325fb14c0f3172a0232397c049552304ec67
socrazys <2803282420@qq.com>
2019-01-03 11:44:39 +0800

    商品

--- src/main/java/com/VipShopApplication.java
+++ src/main/java/com/VipShopApplication.java
@@ -2,12 +2,11 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class VipShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VipShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VipShopApplication.class, args);
+    }
 }
