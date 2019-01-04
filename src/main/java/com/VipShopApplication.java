a3a5f2c82dd672ab7860f1b7e18b694ab9056bf1
socrazys <2803282420@qq.com>
2019-01-04 14:23:50 +0800

    555

--- src/main/java/com/VipShopApplication.java
+++ src/main/java/com/VipShopApplication.java
@@ -2,7 +2,6 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 
 @SpringBootApplication
 public class VipShopApplication {
@@ -10,14 +9,4 @@ public class VipShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(VipShopApplication.class, args);
     }
-=======
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-@SpringBootApplication()
-public class VipShopApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VipShopApplication.class, args);
-	}
->>>>>>> branch 'master' of https://github.com/CLHZ/VipShop.git
 }
